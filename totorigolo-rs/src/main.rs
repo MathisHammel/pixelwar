a1b3b29a6e4d66138e5b21a0f136e556d2ea661a
Thomas Lacroix <toto.rigolo@free.fr>
2020-04-02 00:54:36 +0200

    Move the picture to bottom-right corner

--- totorigolo-rs/src/main.rs
+++ totorigolo-rs/src/main.rs
@@ -6,12 +6,12 @@ fn main() {
 
     let proof_gen = proof::ProofGeneratorBuilder::new()
             .with_prefix("totorigolo-")
-            .with_suffix_length(20)
+            .with_suffix_length(24)
             .with_digest_prefix("00000")
             .build();
 
     loop {
         // paint_pink();
-        paint_image(proof_gen.clone(), Pos::new(2, 20), "images/insalgo-30-rouge.png");
+        paint_image(proof_gen.clone(), Pos::new(100 - 35, 100 - 35), "images/insalgo-30-rouge.png");
     }
 }
